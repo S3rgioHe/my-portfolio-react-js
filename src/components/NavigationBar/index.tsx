8e3cf8307655b9240ceaf836ceda47c5c25151fd
S3rgioHe <sergioextremehe@gmail.com>
2022-06-15 10:56:28 -0300

    feat: create navigation bar

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,12 +1,34 @@
-import {  } from "./style";
+import { Button, ButtonList, Container, Indicator, TitleButton } from "./style";
 import { IoHomeOutline, IoPersonOutline, IoAlbumsOutline, IoPhonePortraitOutline } from "react-icons/io5";
+import { useState } from "react";
 
 
 export function NavigationBar() {
 
+    const [page, setPage] = useState(0);
     return (
         <>
-            
+            <Container>
+                <ButtonList>
+                    <Button active={page === 0} onClick={() => {setPage(0)}}>
+                        <IoHomeOutline className="icon"/>
+                        <TitleButton className="text">Home</TitleButton>
+                    </Button>
+                    <Button active={page === 1} onClick={() => {setPage(1)}}>
+                        <IoPersonOutline className="icon"/>
+                        <TitleButton className="text">Sobre</TitleButton>
+                    </Button>
+                    <Button active={page === 2} onClick={() => {setPage(2)}}>
+                        <IoAlbumsOutline className="icon"/>
+                        <TitleButton className="text">Projetos</TitleButton>
+                    </Button>
+                    <Button active={page === 3} onClick={() => {setPage(3)}}>
+                        <IoPhonePortraitOutline className="icon"/>
+                        <TitleButton className="text">Contato</TitleButton>
+                    </Button>
+                    <Indicator page={page}/>
+                </ButtonList>
+            </Container>
         </>
     )
 }
--- src/components/NavigationBar/style.ts
+++ src/components/NavigationBar/style.ts
@@ -1,1 +1,90 @@
 import styled from 'styled-components';
+import { transform } from 'typescript';
+
+export const Container = styled.div`
+    margin: 20px;
+    width: 310px;
+    height: 70px;
+
+    top: 200px;
+
+    background-color: #fff;
+    box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1);
+
+    position: relative;
+    display: flex;
+
+    border-radius: 10px;
+    margin-left: auto;
+    margin-right: auto;
+
+    transition: 0.5s;
+`;
+
+export const ButtonList = styled.ul`
+    display: flex;
+    padding: 0 20px 0 20px;
+    width: auto;
+`;
+
+interface ButtonProps {
+    active: boolean
+}
+
+export const Button = styled.li<ButtonProps>`
+    position: relative;
+    list-style: none;
+    width: 70px;
+    height: 70px;
+    z-index: 1;
+    justify-content: center;
+    
+    &:hover {
+        cursor: pointer;
+    }
+
+    & .icon {
+
+        position: relative;
+        display: block;
+        line-height: 75px;
+        font-size: 1.5em;
+        transition: 0.5s;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 5%;
+
+        ${props => props.active && 'transform: translateY(-32px)'}
+    }
+
+    & .text {
+        ${props => props.active && 'opacity: 1; transform: translateY(5px);'}
+    }
+`;
+
+export const TitleButton = styled.span`
+    position: absolute;
+    font-weight: 400;
+    font-size: 0.75em;
+    letter-spacing: 0.05em;
+    transition: 0.5s;
+    opacity: 0;
+    transform: translateY(18px);
+    left: 25%;
+`;
+
+interface IndicatorProps {
+    page: number;
+}
+
+export const Indicator = styled.div<IndicatorProps>`
+    position: absolute;
+    top: -50%;
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    background: #820ad1;
+    transition: 0.3s;
+    
+    transform: translateX(${props => props.page * 70}px);
+`;
