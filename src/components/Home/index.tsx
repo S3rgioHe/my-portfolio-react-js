10589d2d3a20bf3e000f3ac0c1f9adc63dcebbb1
S3rgioHe <sergioextremehe@gmail.com>
2022-06-17 15:28:19 -0300

    feat: add home screen

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,23 @@
-import { Container } from "./style";
-
+import { Column, Container, Img, SubTitle, Title } from "./style";
+import Typed from "react-typed";
+import SergioPhoto from "../../assets/imgs/me.png";
 interface HomeProps {
     active: boolean
 }
 
 export function Home({ active }: HomeProps) {
-    return (
-        <Container active={active} >
-            <h1>Home</h1>
-        </Container>
+    return ( 
+        <>
+            <Container active={active} >
+                <Column>
+                    <Title>Sérgio Henrique</Title>
+                    <SubTitle>Desenvolvedor Full Stack</SubTitle>
+                    <Typed className="typed" strings={["Palavra aleatoria", "1"]} typeSpeed={100} loop />
+                </Column>
+                <Column>
+                    <Img src={SergioPhoto}/>
+                </Column>
+            </Container>
+        </>
     );
 }
--- src/components/Home/style.ts
+++ src/components/Home/style.ts
@@ -1,3 +1,4 @@
+import { title } from "process";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -5,12 +6,15 @@ interface ContainerProps {
 }
 
 export const Container = styled.div<ContainerProps>`
-  margin: 20px;
-    width: 310px;
-    height: 70px;
+    margin: 20px;
     position: absolute;
 
-    top: 40%;
+    width: 45%;
+
+    display: grid;
+    grid-template-columns: 2fr 2fr;
+
+    top: 20%;
     margin-left: auto;
     margin-right: auto;
     left: 0;
@@ -21,4 +25,29 @@ export const Container = styled.div<ContainerProps>`
 
     ${props => props.active ? '' : 'dsplay: none;'}
     ${props => props.active ? '' : 'opacity: 0;'}
+`;
+
+export const Column = styled.div`
+  .typed {
+    font-size: 25px;
+  }
+`;
+
+export const Title = styled.h1`
+  margin-top: 40px;
+  font-size: 55px;
+  font-weight: bold;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const Img = styled.img`
+  height: 45%;
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
