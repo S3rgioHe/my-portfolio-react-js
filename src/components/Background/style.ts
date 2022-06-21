7dbf6e909033d19ca528c66c4638153bc6b4f663
S3rgioHe <sergioextremehe@gmail.com>
2022-06-21 17:32:48 -0300

    test: background color

--- src/components/Background/style.ts
+++ src/components/Background/style.ts
@@ -6,11 +6,24 @@ export const Container = styled.div`
     flex-direction: column;
     overflow: hidden;
     background-color: var(--background);
+
 `;
 
 export const Row = styled.div`
     .icon {
         color: grey;
         opacity: 0.1;
+        transition: 4s;
+
+        &:hover {
+            background: #820ad1;
+
+            transition: 0s;
+            color: #fff;
+            box-shadow: 0px 0px 40px px rgba(130,10,209, 0.3);
+            opacity: 1;
+
+            border-radius: 50%;
+        }
     }
 `;
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,13 +1,33 @@
 import { Container } from "./style"
+import Carousel from "react-elastic-carousel";
+import { useState } from "react";
 
 interface ProjectsProps {
     active: boolean
 }
 
 export function Projects({ active }: ProjectsProps) {
+
+    const [items, setItems] = useState([1, 2, 3, 4]);
+
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+        { width: 768, itemsToShow: 3 },
+        { width: 1200, itemsToShow: 4 }
+    ];
+
     return (
         <Container active={active}>
             <h1>Teste</h1>
+            <hr className="seperator" />
+            <div className="carousel-wrapper">
+                {/* <Carousel breakPoints={breakPoints}>
+                    {items.map((item) => (
+                        <Item key={item}>{item}</Item>
+                    ))}
+                </Carousel> */}
+            </div>
         </Container>
     )
 }
