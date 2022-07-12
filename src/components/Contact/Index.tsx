127e0332a606dc2082a3ed73e206d5ce56a861f1
S3rgioHe <sergioextremehe@gmail.com>
2022-07-12 14:01:28 -0300

    feat: links to contact

--- src/components/Contact/Index.tsx
+++ src/components/Contact/Index.tsx
@@ -11,21 +11,30 @@ export function Contact( { active }: ContactProps ) {
     return (
         <Container active={active}>
 
-            <Title>Contact & links </Title>
+            <Title>Contato</Title>
             <Content>
                 <List>
                     <Item>
                         <MdOutlineEmail className="icon" />
-                        <h2>Email</h2>
+                        <h2>Email -</h2>
+                        <a href="mailto:sergiohenriquefn@gmail.com" target="_blank">
+                            sergiohenriquefn@gmail.com
+                        </a>
+
                     </Item>
                     <Item> 
                         <MdSmartphone className="icon"/>
-                        <h2>Phone</h2>
-                        <p>+55 (11) 99999-9999</p>
+                        <h2>Phone -</h2>
+                        <a href="tel:+5581999058789">
+                            +55 (81) 9990-58789
+                        </a>
                     </Item>
                     <Item>
                         <BsLinkedin className="icon"/>
-                        <h2>Linkedin</h2>
+                        <h2>Linkedin -</h2>
+                        <a href="https://www.linkedin.com/in/sergio-henrique-7b436a209/" target="_blank">
+                            Sérgio Henrique
+                        </a>
                     </Item>
                 </List>
                 <img src={qrcode} alt="qrcode" />
